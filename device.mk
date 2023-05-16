6355aaffed813efbbc0713fd922da361b41e9180
anupam41 <anupamsamanta41@gmail.com>
2023-05-16 08:34:15 +0000

    sweet: Define TARGET_BOARD_PLATFORM in device makefile

--- device.mk
+++ device.mk
@@ -346,6 +346,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Platform
+MSMSTEPPE := sm6150
+TARGET_BOARD_PLATFORM := $(MSMSTEPPE)
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
