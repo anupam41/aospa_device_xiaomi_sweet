a8f4c268d4b6783dccf200df2dbc5f9e3d61d846
anupam41 <anupamsamanta41@gmail.com>
2023-05-31 09:06:30 +0530

    audio

--- device.mk
+++ device.mk
@@ -39,6 +39,11 @@ PRODUCT_PACKAGES += \
     fs_config_files
 
 # Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio.service
+    
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
