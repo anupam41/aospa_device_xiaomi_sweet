9a7391a23be1e294156eb2cc69662d0f06d5435a
anupam41 <anupamsamanta41@gmail.com>
2023-06-20 19:34:14 +0530

    sss

--- device.mk
+++ device.mk
@@ -148,15 +148,13 @@ PRODUCT_PACKAGES += \
 
 # Init scripts
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
-    $(LOCAL_PATH)/rootdir/etc/init.qcom.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/etc/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.rc \
+    $(LOCAL_PATH)/rootdir/etc/ueventd.sweet.rc:$(TARGET_COPY_OUT_ODM)/etc/ueventd.rc \
     $(LOCAL_PATH)/rootdir/etc/init.sweet.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.sweet.rc
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/bin/init.qcom.post_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.post_boot.sh \
-    $(LOCAL_PATH)/rootdir/bin/init.qcom.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.sh \
-    $(LOCAL_PATH)/rootdir/bin/init.qti.early_init.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.early_init.sh
+    
+PRODUCT_COPY_FILES += \    
+    $(LOCAL_PATH)/rootdir/bin/init.sweet.post_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.sweet.post_boot.sh
+   
 
 # IRSC
 PRODUCT_COPY_FILES += \
@@ -245,6 +243,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     bt \
     charging \
     display \
+    init \
     keymaster \
     media \
     overlay \
@@ -259,9 +258,6 @@ TARGET_COMMON_QTI_COMPONENTS := \
 # Recovery
 PRODUCT_PACKAGES += \
     fastbootd
-
-PRODUCT_PACKAGES += \
-    init.recovery.qcom.rc
 
 # Sensors
 PRODUCT_PACKAGES += \
@@ -281,10 +277,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
-
+    
 # Uevent
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
+    $(LOCAL_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc    
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
