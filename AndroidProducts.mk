6996a2f3149b97811eed7d1da08cdc30512968c1
anupam41 <anupamsamanta41@gmail.com>
2023-12-20 18:05:02 +0530

    sweet: statix bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/statix_sweet.mk
+
+COMMON_LUNCH_CHOICES := \
+    statix_sweet-eng \
+    statix_sweet-user \
+    statix_sweet-userdebug
